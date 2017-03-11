b420cd20281139a76361d0df810221883512d7d0
Jente Hidskes <hjdskes@gmail.com>
2017-03-11 20:35:15 +0100

    modules: add LogModule

---
+++ module/logmodule.go
@@ -0,0 +1,42 @@
+package module
+
+import (
+	"fmt"
+	"log"
+)
+
+type LogModule struct {
+}
+
+func (m LogModule) Topics() []string {
+	return []string{"notice", "error"}
+}
+
+func (m LogModule) Receive(args []interface{}) {
+	cat, ok := args[0].(string)
+	if !ok {
+		log.Println("LogModule received data that didn't start with a category")
+		cat = "notice"
+	}
+
+	msg, ok := args[1].(string)
+	if !ok {
+		log.Println("LogModule received data that wasn't a valid string")
+		return
+	}
+
+	switch cat {
+	case "notice":
+		m.logNotice(msg)
+	case "error":
+		m.logError(msg)
+	}
+}
+
+func (m LogModule) logNotice(msg string) {
+	fmt.Println("WARNING: ", msg)
+}
+
+func (m LogModule) logError(msg string) {
+	fmt.Println("ERROR: ", msg)
+}
